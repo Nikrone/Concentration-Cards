75360c2475291873ff88c8d7d25ec4f6f60919b0
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-08-31 16:58:28 +0300

    feat: add UI and logic App

--- Concentration Cards/ViewController.swift
+++ Concentration Cards/ViewController.swift
@@ -8,11 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBOutlet weak var touchLabel: UILabel!
+    
+    var touches = 0 {
+//        Observer - следит за изменением значения переменной touches, если она изменяется, то выполняет код (touchLabel.text = "Touches: \(touches)")
+        didSet {
+            touchLabel.text = "Touches: \(touches)"
+        }
     }
+    
+    func flipButton(emoji: String, button: UIButton) {
+//      currentTitle - заголовок который видим в данный момент
+        if button.currentTitle == emoji {
+//      setTitle - заголовок который назначаем
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .orange
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = .white
+        }
+    }
+    
+let emojiCollection = ["🦊", "🐰", "🦊", "🐰"]
+
+    
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+//        безопасное разворачиваение опционала (Optional Binding)
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+        }
+    }
+    
 
 
 }
